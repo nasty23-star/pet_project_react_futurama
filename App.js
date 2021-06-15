826ead2ca00eca9c4257199800d760250b2e6220
nasty23-star <71438930+nasty23-star@users.noreply.github.com>
2021-06-15 21:11:22 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+
+
+import './App.css';
+import HoverToad from './HoverToad';
+import Name from './Name';
+import QuizFuturama from './QuizFuturama';
+
+function App() {
+  return (
+    <div style={{ backgroundColor: "rgb(78, 140, 190)" }}>
+      <HoverToad />
+      <Name />
+      <QuizFuturama />
+     
+    </div>
+  );
+}
+
+export default App;
